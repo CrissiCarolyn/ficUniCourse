fdffec2d083c7a627a5bef92758454c75d01c353
CrissiCarolyn <cnimmakuri@gmail.com>
2022-11-08 16:02:27 +0000

    changes1

--- wp-content/themes/fictional-uni-theme/inc/like-posts-route.php
+++ wp-content/themes/fictional-uni-theme/inc/like-posts-route.php
@@ -55,7 +55,7 @@ function deleteLikePost($data) {
   $likeId = sanitize_text_field($data['like']);
   if (get_current_user_id() == get_post_field('post_author', $likeId) AND get_post_type($likeId) == 'like') {
     wp_delete_post($likeId, true);
-    return 'Congrats, like deleted.';
+    return 'Like deleted.';
   } else {
     die("You do not have permission to delete that.");
   }
